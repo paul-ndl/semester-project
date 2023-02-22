1a646179c13fad5cd287f508beb8280ebabfc210
jules20022000 <jules.maglione@epfl.ch>
2023-02-22 15:55:56 +0100

    create HIT with Python

--- mturk/CreateHitSample.py
+++ mturk/CreateHitSample.py
@@ -14,6 +14,12 @@
 
 import sys
 import boto3
+import key
+
+region_name = "us-east-1"
+aws_access_key_id = key.id
+aws_secret_access_key = key.key
+
 
 # Before connecting to MTurk, set up your AWS account and IAM settings as
 # described here:
@@ -31,69 +37,75 @@ import boto3
 create_hits_in_live = False
 
 environments = {
-        "live": {
-            "endpoint": "https://mturk-requester.us-east-1.amazonaws.com",
-            "preview": "https://www.mturk.com/mturk/preview",
-            "manage": "https://requester.mturk.com/mturk/manageHITs",
-            "reward": "0.00"
-        },
-        "sandbox": {
-            "endpoint": "https://mturk-requester-sandbox.us-east-1.amazonaws.com",
-            "preview": "https://workersandbox.mturk.com/mturk/preview",
-            "manage": "https://requestersandbox.mturk.com/mturk/manageHITs",
-            "reward": "0.11"
-        },
+    "live": {
+        "endpoint": "https://mturk-requester.us-east-1.amazonaws.com",
+        "preview": "https://www.mturk.com/mturk/preview",
+        "manage": "https://requester.mturk.com/mturk/manageHITs",
+        "reward": "0.00",
+    },
+    "sandbox": {
+        "endpoint": "https://mturk-requester-sandbox.us-east-1.amazonaws.com",
+        "preview": "https://workersandbox.mturk.com/mturk/preview",
+        "manage": "https://requestersandbox.mturk.com/mturk/manageHITs",
+        "reward": "0.0",
+    },
 }
-mturk_environment = environments["live"] if create_hits_in_live else environments["sandbox"]
+mturk_environment = (
+    environments["live"] if create_hits_in_live else environments["sandbox"]
+)
 
 # use profile if one was passed as an arg, otherwise
-profile_name = sys.argv[1] if len(sys.argv) >= 2 else None
-session = boto3.Session(profile_name=profile_name)
-client = session.client(
-    service_name='mturk',
-    region_name='us-east-1',
-    endpoint_url=mturk_environment['endpoint'],
+client = boto3.client(
+    service_name="mturk",
+    endpoint_url=mturk_environment["endpoint"],
+    region_name=region_name,
+    aws_access_key_id=aws_access_key_id,
+    aws_secret_access_key=aws_secret_access_key,
 )
 
 # Test that you can connect to the API by checking your account balance
 user_balance = client.get_account_balance()
 
 # In Sandbox this always returns $10,000. In live, it will be your acutal balance.
-print "Your account balance is {}".format(user_balance['AvailableBalance'])
+print("Your account balance is {}".format(user_balance["AvailableBalance"]))
 
 # The question we ask the workers is contained in this file.
-question_sample = open("my_question.xml", "r").read()
+question_sample = open("question.xml", "r").read()
+# question_sample = '<?xml version="1.0" encoding="UTF-8"?><QuestionForm xmlns="http://mechanicalturk.amazonaws.com/AWSMechanicalTurkDataSchemas/2005-10-01/QuestionForm.xsd"><Overview><Title>Answer a simple question</Title></Overview><Question><QuestionIdentifier>question1</QuestionIdentifier><DisplayName>What is your favorite color?</DisplayName><IsRequired>true</IsRequired><QuestionContent><Text>Choose your favorite color from the options below:</Text></QuestionContent><AnswerSpecification><SelectionAnswer><StyleSuggestion>radiobutton</StyleSuggestion><Selections><Selection><SelectionIdentifier>red</SelectionIdentifier><Text>Red</Text></Selection><Selection><SelectionIdentifier>blue</SelectionIdentifier><Text>Blue</Text></Selection><Selection><SelectionIdentifier>green</SelectionIdentifier><Text>Green</Text></Selection><Selection><SelectionIdentifier>yellow</SelectionIdentifier><Text>Yellow</Text></Selection></Selections></SelectionAnswer></AnswerSpecification></Question></QuestionForm>'
+
 
 # Example of using qualification to restrict responses to Workers who have had
 # at least 80% of their assignments approved. See:
 # http://docs.aws.amazon.com/AWSMechTurk/latest/AWSMturkAPI/ApiReference_QualificationRequirementDataStructureArticle.html#ApiReference_QualificationType-IDs
-worker_requirements = [{
-    'QualificationTypeId': '000000000000000000L0',
-    'Comparator': 'GreaterThanOrEqualTo',
-    'IntegerValues': [80],
-    'RequiredToPreview': True,
-}]
+worker_requirements = [
+    {
+        "QualificationTypeId": "000000000000000000L0",
+        "Comparator": "GreaterThanOrEqualTo",
+        "IntegerValues": [80],
+        "RequiredToPreview": True,
+    }
+]
 
 # Create the HIT
 response = client.create_hit(
     MaxAssignments=3,
     LifetimeInSeconds=600,
     AssignmentDurationInSeconds=600,
-    Reward=mturk_environment['reward'],
-    Title='Answer a simple question',
-    Keywords='question, answer, research',
-    Description='Answer a simple question. Created from mturk-code-samples.',
+    Reward=mturk_environment["reward"],
+    Title="Answer a simple question",
+    Keywords="question, answer, research",
+    Description="Answer a simple question. Created from mturk-code-samples.",
     Question=question_sample,
     QualificationRequirements=worker_requirements,
 )
 
 # The response included several fields that will be helpful later
-hit_type_id = response['HIT']['HITTypeId']
-hit_id = response['HIT']['HITId']
-print "\nCreated HIT: {}".format(hit_id)
+hit_type_id = response["HIT"]["HITTypeId"]
+hit_id = response["HIT"]["HITId"]
+print("\nCreated HIT: {}".format(hit_id))
 
-print "\nYou can work the HIT here:"
-print mturk_environment['preview'] + "?groupId={}".format(hit_type_id)
+print("\nYou can work the HIT here:")
+print(mturk_environment["preview"] + "?groupId={}".format(hit_type_id))
 
-print "\nAnd see results here:"
-print mturk_environment['manage']
+print("\nAnd see results here:")
+print(mturk_environment["manage"])
--- mturk/RetrieveAndApproveHitSample.py
+++ mturk/RetrieveAndApproveHitSample.py
@@ -15,6 +15,11 @@
 import sys
 import boto3
 from xml.dom.minidom import parseString
+import key
+
+region_name = "us-east-1"
+aws_access_key_id = key.id
+aws_secret_access_key = key.key
 
 # Before connecting to MTurk, set up your AWS account and IAM settings as described here:
 # https://blog.mturk.com/how-to-use-iam-to-control-api-access-to-your-mturk-account-76fe2c2e66e2
@@ -26,7 +31,7 @@ from xml.dom.minidom import parseString
 # Sign up for a Sandbox account at https://requestersandbox.mturk.com/ with the same credentials as your main MTurk account.
 
 # This HIT id should be the HIT you just created - see the CreateHITSample.py file for generating a HIT
-if(len(sys.argv) < 2):
+if len(sys.argv) < 2:
     print("You must pass a HIT id as the first argument")
     sys.exit(-1)
 
@@ -36,58 +41,68 @@ hit_id = sys.argv[1]
 create_hits_in_live = False
 
 environments = {
-        "live": {
-            "endpoint": "https://mturk-requester.us-east-1.amazonaws.com",
-            "preview": "https://www.mturk.com/mturk/preview",
-            "manage": "https://requester.mturk.com/mturk/manageHITs",
-            "reward": "0.00"
-        },
-        "sandbox": {
-            "endpoint": "https://mturk-requester-sandbox.us-east-1.amazonaws.com",
-            "preview": "https://workersandbox.mturk.com/mturk/preview",
-            "manage": "https://requestersandbox.mturk.com/mturk/manageHITs",
-            "reward": "0.11"
-        },
+    "live": {
+        "endpoint": "https://mturk-requester.us-east-1.amazonaws.com",
+        "preview": "https://www.mturk.com/mturk/preview",
+        "manage": "https://requester.mturk.com/mturk/manageHITs",
+        "reward": "0.00",
+    },
+    "sandbox": {
+        "endpoint": "https://mturk-requester-sandbox.us-east-1.amazonaws.com",
+        "preview": "https://workersandbox.mturk.com/mturk/preview",
+        "manage": "https://requestersandbox.mturk.com/mturk/manageHITs",
+        "reward": "0.00",
+    },
 }
-mturk_environment = environments["live"] if create_hits_in_live else environments["sandbox"]
+mturk_environment = (
+    environments["live"] if create_hits_in_live else environments["sandbox"]
+)
 
 # use profile if one was passed as an arg, otherwise
 profile_name = sys.argv[2] if len(sys.argv) >= 3 else None
-session = boto3.Session(profile_name=profile_name)
-client = session.client(
-    service_name='mturk',
-    region_name='us-east-1',
-    endpoint_url=mturk_environment['endpoint'],
+
+client = boto3.client(
+    service_name="mturk",
+    endpoint_url=mturk_environment["endpoint"],
+    region_name=region_name,
+    aws_access_key_id=aws_access_key_id,
+    aws_secret_access_key=aws_secret_access_key,
 )
 
 hit = client.get_hit(HITId=hit_id)
-print 'Hit {} status: {}'.format(hit_id, hit['HIT']['HITStatus'])
+print("Hit {} status: {}".format(hit_id, hit["HIT"]["HITStatus"]))
 response = client.list_assignments_for_hit(
     HITId=hit_id,
-    AssignmentStatuses=['Submitted', 'Approved'],
+    AssignmentStatuses=["Submitted", "Approved"],
     MaxResults=10,
 )
 
-assignments = response['Assignments']
-print 'The number of submitted assignments is {}'.format(len(assignments))
+assignments = response["Assignments"]
+print("The number of submitted assignments is {}".format(len(assignments)))
 for assignment in assignments:
-    worker_id = assignment['WorkerId']
-    assignment_id = assignment['AssignmentId']
-    answer_xml = parseString(assignment['Answer'])
+    worker_id = assignment["WorkerId"]
+    assignment_id = assignment["AssignmentId"]
+    answer_xml = parseString(assignment["Answer"])
 
     # the answer is an xml document. we pull out the value of the first
     # //QuestionFormAnswers/Answer/FreeText
-    answer = answer_xml.getElementsByTagName('FreeText')[0]
+    answer = answer_xml.getElementsByTagName("FreeText")[0]
     # See https://stackoverflow.com/questions/317413
-    only_answer = " ".join(t.nodeValue for t in answer.childNodes if t.nodeType == t.TEXT_NODE)
+    only_answer = " ".join(
+        t.nodeValue for t in answer.childNodes if t.nodeType == t.TEXT_NODE
+    )
 
-    print 'The Worker with ID {} submitted assignment {} and gave the answer "{}"'.format(worker_id, assignment_id, only_answer)
+    print(
+        'The Worker with ID {} submitted assignment {} and gave the answer "{}"'.format(
+            worker_id, assignment_id, only_answer
+        )
+    )
 
     # Approve the Assignment (if it hasn't already been approved)
-    if assignment['AssignmentStatus'] == 'Submitted':
-        print 'Approving Assignment {}'.format(assignment_id)
+    if assignment["AssignmentStatus"] == "Submitted":
+        print("Approving Assignment {}".format(assignment_id))
         client.approve_assignment(
             AssignmentId=assignment_id,
-            RequesterFeedback='good',
+            RequesterFeedback="good",
             OverrideRejection=False,
         )
