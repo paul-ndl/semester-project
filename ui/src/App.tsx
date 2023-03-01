b71959469d66058490a0d66f17b3b22fc04e8e95
Paul Nadal <paul.nadal@epfl.ch>
2023-03-01 10:56:35 +0100

    create first element

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,23 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Comparison1 from './Components/Comparison1';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Comparison1/>
       </header>
     </div>
   );
